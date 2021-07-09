9e7d12043b54837cce4dca179804b8f713415515
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-07-09 16:10:42 -0700

    converted into object-oriented program

--- main.py
+++ main.py
@@ -3,66 +3,72 @@ import time
 # module that contains constants frequently used in pygame
 from pygame.locals import *
 
+class Snake:
+    def __init__(self, parent_screen):
+        self.parent_screen = parent_screen
+        self.block = pygame.image.load('resources/block.jpeg').convert()
+        # draw image onto another
+        self.xblock = 100
+        self.yblock = 100
+    
+    def draw(self):
+        self.parent_screen.fill((200, 125, 5))
+        self.parent_screen.blit(self.block, (self.xblock, self.yblock))
+         # update the full display surface to the screen
+        pygame.display.flip()
+    
+    def move_left(self):
+        self.xblock = self.xblock - 10
+        self.draw()
+
+    def move_right(self):
+        self.xblock = self.xblock + 10
+        self.draw()
+
+    def move_up(self):
+        self.yblock = self.yblock - 10
+        self.draw()
+
+    def move_down(self):
+        self.yblock = self.yblock + 10
+        self.draw()
+
 class Game:
     def __init__(self):
-        pass
+        pygame.init()
+        # initialize window / screen for display
+        self.surface = pygame.display.set_mode((500, 500))
+        # color
+        self.surface.fill((200, 125, 5))
+        self.snake = Snake(self.surface)
+        self.snake.draw()
 
     def run(self):
-        pass
-    
+        running = True
+
+        while running == True: 
+            # pygame.event.get() will 
+            # get events from queue
+            for event in pygame.event.get():
+                # if key is clicked
+                if event.type == KEYDOWN:
+                    # if key happens to be escape key
+                    if event.key == K_ESCAPE:
+                        running = False
+
+                    if event.key == K_UP:
+                        self.snake.move_up()
+                    if event.key == K_DOWN:
+                        self.snake.move_down()
+                    if event.key == K_RIGHT:
+                        self.snake.move_right()
+                    if event.key == K_LEFT:
+                        self.snake.move_left()
+                # exit button is clicked on module
+                elif event.type == QUIT:
+                    running = False
+
 if __name__ == "__main__":
     game = Game()
 
     game.run()
-
-    pygame.init()
-    # initialize window / screen for display
-    surface = pygame.display.set_mode((1000, 500))
-    # color
-    surface.fill((200, 125, 5))
-    
-    block = pygame.image.load('resources/block.jpeg').convert()
-    # draw image onto another
-    xblock = 100
-    yblock = 100
-
-    surface.blit(block, (xblock, yblock))
-    # update the full display surface to the screen
-    pygame.display.flip()
-
-    def draw_block():
-        surface.fill((200, 125, 5))
-
-        surface.blit(block, (xblock, yblock))
-        
-        pygame.display.flip()
-
-    running = True
-
-    while running == True: 
-        # pygame.event.get() will 
-        # get events from queue
-        for event in pygame.event.get():
-            # if key is clicked
-            if event.type == KEYDOWN:
-                # if key happens to be escape key
-                if event.key == K_ESCAPE:
-                    running = False
-
-                if event.key == K_UP:
-                    yblock = yblock - 10
-                    draw_block()
-                if event.key == K_DOWN:
-                    yblock = yblock + 10
-                    draw_block()
-                if event.key == K_RIGHT:
-                    xblock = xblock + 10
-                    draw_block()
-                if event.key == K_LEFT:
-                    xblock = xblock - 10
-                    draw_block()
-            # exit button is clicked on module
-            elif event.type == QUIT:
-                running = False
-
-
